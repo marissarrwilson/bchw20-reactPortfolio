1e912122b029aa1a2ccecceccec206c02669003d
Mars <wilson.marissarose@gmail.com>
2020-12-07 21:11:39 -0500

    contact changes

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,40 +1,48 @@
 import React from "react";
 
-function Contact (props) {
+function Contact(props) {
   return (
     <div>
-      <h1><center>Contact</center></h1>
+      <h1>
+        <center>Contact</center>
+      </h1>
       <form id="fullform">
-
-      <ul>
-            <li>
-              <i class="fab fa-linkedin"></i>
-              <a
-                href="https://www.linkedin.com/in/marissa-wilson-44578217a/"
-                target="_blank"
-              > Linkedin</a>
-            </li>
-            <li>
-            <i class="fab fa-github"></i> 
-               <a href="https://github.com/marissarrwilson" target="_blank"> Github</a>
-            </li>
-            <li>
-            <i class="far fa-file"></i> 
-              <a
-                href="https://docs.google.com/document/d/1HdLLGfZGL63RfK2XURthGTctJve-56r5zIhcOFslMWc/edit"
-                target="_blank"
-              > Resume
-              </a>
-            </li>
+        <ul>
           <li>
-          <i class="far fa-envelope"></i> 
-          <a href="mailto:wilson.marissarose@gmail.com" target="_blank"> Email</a>
+            <i class="fab fa-linkedin"></i>
+            <a
+              href="https://www.linkedin.com/in/marissa-wilson-44578217a/"
+              target="_blank"
+            >
+              {" "}
+              Linkedin
+            </a>
           </li>
-          </ul>
-       
-       
-
-        
+          <li>
+            <i class="fab fa-github"></i>
+            <a href="https://github.com/marissarrwilson" target="_blank">
+              {" "}
+              Github
+            </a>
+          </li>
+          <li>
+            <i class="far fa-file"></i>
+            <a
+              href="https://docs.google.com/document/d/1HdLLGfZGL63RfK2XURthGTctJve-56r5zIhcOFslMWc/edit"
+              target="_blank"
+            >
+              {" "}
+              Resume
+            </a>
+          </li>
+          <li>
+            <i class="far fa-envelope"></i>
+            <a href="mailto:wilson.marissarose@gmail.com" target="_blank">
+              {" "}
+              Email
+            </a>
+          </li>
+        </ul>
       </form>
     </div>
   );
